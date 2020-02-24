d6e28fa6e4bdde5e241aa942e7dfae9be4ee01e3
nom <deathdisco@protonmail.com>
2020-02-24 17:15:46 +1100

    btc config

--- system/configuration.nix
+++ system/configuration.nix
@@ -34,6 +34,7 @@
   fileSystems = {
     "/drives/Windows".device = "/dev/disk/by-label/Windows";
     "/drives/Data".device = "/dev/disk/by-label/Data";
+    "/drives/External".device = "/dev/disk/by-uuid/42B64D06120CD207";
   };
 
   # FONTS
