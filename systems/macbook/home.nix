50867d1363d530418fe420980453b1ab56052fb0
Rob Saunders <rob@wearebrandnew.com>
2020-05-10 23:58:26 +1000

    macos config

--- systems/macbook/home.nix
+++ systems/macbook/home.nix
@@ -1,31 +1,34 @@
 # macos home-manager configuration
 #
 #   home-manager switch --file ./home.nix
+#   brew install $(cat homebrew.txt)
 #
 { config, pkgs, ... }:
 let
 in {
-    imports = [
-        ../../applications/alacritty.nix
+  imports = [ ../../applications/alacritty.nix ];
+
+  programs.bash.sessionVariables.EDITOR = "vim";
+
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    environment.variables = {
+      EDITOR = "vim";
+      PATH = "~/.cargo/bin:$PATH";
+    };
+  };
+
+  home = {
+    packages = [
+      # NIX
+      pkgs.nixfmt
+
+      # CLI
+      pkgs.ranger
+      pkgs.gnupg1compat # gpg encryption
+
+      # GUI
     ];
-
-    programs.bash.sessionVariables.EDITOR = "vim";
-
-    nixpkgs.config = {
-        allowUnfree = true;
-
-        environment.variables = {
-        EDITOR = "vim";
-        PATH = "~/.cargo/bin:$PATH";
-        };
-    };
-
-    home = {
-        packages = [
-            # CLI
-            pkgs.ranger
-
-            # GUI
-        ];
-    };
+  };
 }
